efd6eb10c02db784946528d3421d1456ce283d6e
musa <mediggggh@gmail.com>
2024-06-01 18:33:51 +0300

    Добавил условие если курсы не прогрузились

--- src/Components/Courses/CoursesContainer.tsx
+++ src/Components/Courses/CoursesContainer.tsx
@@ -12,9 +12,9 @@ const CoursesContainerComponent: React.FC<CoursesContainer> = (props) => {
 
   return (
     <div className="courses">
-      {courses.map((course) => {
+      {courses.length !== 0 ? courses.map((course) => {
         return <Course key={course.id} id={course.id} name={course.name} image={course.image} bgColor={course.bgColor} />;
-      })}
+      }) : 'В данный момент не курсов для загрузки'}
     </div>
   );
 };
